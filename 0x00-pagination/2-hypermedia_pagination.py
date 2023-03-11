c39ba59faa3ec80b91fa6d503e66f06d10545860
andrew21-mch <nfonandrew73@gmail.com>
2023-03-11 20:07:06 +0100

    hyperpagination"

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
-""" implemented a Server class to work with data """
+'''
+    Simple pagination.
+'''
 import csv
 import math
 from typing import List
+
+
+def index_range(page, page_size):
+    '''
+        Returns the range of indexes for a given page.
+    '''
+    start = (page - 1) * page_size
+    end = page * page_size
+    return start, end
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -11,7 +24,7 @@ class Server:
     def __init__(self):
         self.__dataset = None
 
-    def get_dataset(self) -> List[List]:
+    def dataset(self) -> List[List]:  # sourcery skip: identity-comprehension
         """Cached dataset
         """
         if self.__dataset is None:
@@ -22,42 +35,42 @@ class Server:
 
         return self.__dataset
 
-    def index_range(self, page: int, page_size: int) -> tuple:
-        """ create a tuple containing page and the page_size """
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return (start_index, end_index)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ get items in a page """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
+        '''
+            Returns a page of data.
+        '''
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        self.dataset()
 
-        start, end = self.index_range(page, page_size)
+        if self.dataset() is None:
+            return []
 
-        data = self.get_dataset()
+        indexRange = index_range(page, page_size)
+        return self.dataset()[indexRange[0]:indexRange[1]]
 
-        list_result = []
-
-        if start >= len(data):
-            return list_result
-
-        return data[start:end]
-
-    def get_hyper(self, page: int, page_size: int) -> dict:
-        """ return dictionary containing data returned"""
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        # sourcery skip: inline-immediately-returned-variable
+        '''
+            Returns info about datset.
+        '''
         data = self.get_page(page, page_size)
-        size_all_pages = math.ceil(len(self.get_dataset()) / page_size)
-        next_page = page + 1 if page + 1 < size_all_pages else None
-        prev_page = page - 1 if page > 1 else None
+        dataSet = self.__dataset
+        lenSet = len(dataSet) if dataSet else 0
 
-        hyper_data = {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": size_all_pages,
+        totalPages = math.ceil(lenSet / page_size) if dataSet else 0
+        page_size = len(data) if data else 0
+
+        prevPage = page - 1 if page > 1 else None
+        nextPage = page + 1 if page < totalPages else None
+
+        hyperMedia = {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': nextPage,
+            'prev_page': prevPage,
+            'total_pages': totalPages
         }
 
-        return hyper_data
+        return hyperMedia
